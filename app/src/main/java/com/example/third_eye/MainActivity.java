0740cc6a048eb85af08d2be8953619b227582fd7
nxnadim <nxnadim007@gmail.com>
2022-07-30 12:24:45 +0600

    Hello

---
+++ app/src/main/java/com/example/third_eye/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.third_eye;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        getSupportActionBar().hide();
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i=new Intent(MainActivity.this,Dashboard.class);
+                startActivity(i);
+                finish();
+            }
+        },3000);
+    }
+}
